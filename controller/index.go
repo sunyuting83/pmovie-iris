658a13022f5a96977c3d27c4488c807a9e0b2de0
sleepsun <sleepsun123@163.com>
2020-06-09 17:32:20 +0800

    index api is done

--- controller/index.go
+++ controller/index.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"fmt"
 	model "pornplay/models"
+	"strings"
 
 	"github.com/kataras/iris"
 )
@@ -11,9 +12,22 @@ var category model.Category
 
 // Indexs index data
 func Indexs(ctx iris.Context) {
+	// fmt.Println(ctx.Host())
+	var datalen int
 	data, err := category.GetIndexs()
+	if len(data) <= 0 {
+		datalen = 0
+	} else {
+		datalen = len(data)
+	}
+	d := make([]model.Category, datalen)
 	if err != nil {
 		fmt.Println("err")
 	}
-	ctx.JSON(iris.Map{"category": data})
+	host := strings.Join([]string{"http", ctx.Host()}, "://")
+	for _, item := range data {
+		item.Cover = strings.Join([]string{host, item.Cover}, "")
+		d = append(d, model.Category{ID: item.ID, Category: item.Category, Sort: item.Sort, Cover: item.Cover})
+	}
+	ctx.JSON(iris.Map{"category": d})
 }
