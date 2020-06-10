fab3b54691a29a757f1f61573e9749ce4417e00e
sun <sleepsun123@163.com>
2020-06-11 00:54:01 +0800

    add cors

--- router/router.go
+++ router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	controller "pornplay/controller"
 
+	"github.com/iris-contrib/middleware/cors"
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/middleware/logger"
 	"github.com/kataras/iris/middleware/recover"
@@ -12,12 +13,16 @@ import (
 func InitRouter() *iris.Application {
 	app := iris.New()
 	// app := iris.Default()
+	crs := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"}, //允许通过的主机名称
+		AllowCredentials: true,
+	})
 
 	app.Use(recover.New())
 	app.Use(logger.New())
 	app.HandleDir("/cover", "./static/cover") // static
 	// Simple group: v1.
-	v1 := app.Party("/api")
+	v1 := app.Party("/api", crs).AllowMethods(iris.MethodOptions) // <- 对于预检很重要。
 	{
 		v1.Get("/", controller.Indexs)
 		v1.Get("/category", controller.CategoryList)
