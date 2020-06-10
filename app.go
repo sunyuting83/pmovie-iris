4154c279742812886eda76f3fcf399195065fd93
sun <sleepsun123@163.com>
2020-06-11 01:08:09 +0800

    add sync.WaitGroup

--- app.go
+++ app.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	stdCtx "context"
 	"flag"
 	router "pornplay/router"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/kataras/iris"
 )
@@ -13,5 +16,20 @@ func main() {
 	flag.StringVar(&port, "p", "3000", "端口号，默认为3000")
 	flag.Parse()
 	app := router.InitRouter()
+	// 优雅的关闭程序
+	serverWG := new(sync.WaitGroup)
+	defer serverWG.Wait()
+
+	iris.RegisterOnInterrupt(func() {
+		serverWG.Add(1)
+		defer serverWG.Done()
+
+		ctx, cancel := stdCtx.WithTimeout(stdCtx.Background(), 20*time.Second)
+		defer cancel()
+
+		// 关闭所有主机
+		app.Shutdown(ctx)
+	})
+
 	app.Run(iris.Addr(strings.Join([]string{":", port}, "")), iris.WithoutServerError(iris.ErrServerClosed))
 }
