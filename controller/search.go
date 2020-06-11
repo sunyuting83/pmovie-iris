d4aa6cf146b95ff6371dc8265d46253a6c13804d
sun <sleepsun123@163.com>
2020-06-11 23:55:01 +0800

    add cache to searchkey

--- controller/search.go
+++ controller/search.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"fmt"
+	leveldb "pornplay/leveldb"
 	"strings"
 
 	"github.com/kataras/iris"
@@ -12,35 +13,41 @@ func SearchKey(ctx iris.Context) {
 	// fmt.Println(ctx.Host())
 	key := ctx.URLParamTrim("key")
 
-	data, err := categoryList.SearchKey(key)
-	if err != nil {
-		fmt.Println("err")
-	}
 	d := make([]Category, 0)
-	for _, item := range data {
-		more := CategoryTojson(item.More)
-		// fmt.Println(more, item.More)
-		if !strings.Contains(item.Cover, "http") {
-			item.Cover = strings.Join([]string{"https", item.Cover}, ":")
+	cache := leveldb.GetLevel(strings.Join([]string{"searchkey", key}, ":"))
+	if cache == "leveldb: not found" {
+		data, err := categoryList.SearchKey(key)
+		if err != nil {
+			fmt.Println("err")
 		}
-		if len(more.CIM) > 0 {
-			if !strings.Contains(more.CIM, "http") {
-				more.CIM = strings.Join([]string{"https", more.CIM}, ":")
+		for _, item := range data {
+			more := CategoryTojson(item.More)
+			// fmt.Println(more, item.More)
+			if !strings.Contains(item.Cover, "http") {
+				item.Cover = strings.Join([]string{"https", item.Cover}, ":")
 			}
-		}
-		if len(more.Play) > 0 {
-			if !strings.Contains(more.Play, "http") {
-				more.Play = strings.Join([]string{"http", more.Play}, ":")
+			if len(more.CIM) > 0 {
+				if !strings.Contains(more.CIM, "http") {
+					more.CIM = strings.Join([]string{"https", more.CIM}, ":")
+				}
 			}
+			if len(more.Play) > 0 {
+				if !strings.Contains(more.Play, "http") {
+					more.Play = strings.Join([]string{"http", more.Play}, ":")
+				}
+			}
+			d = append(d, Category{
+				ID:     item.ID,
+				CID:    item.CID,
+				Title:  item.Title,
+				Cover:  item.Cover,
+				Region: item.Region,
+				More:   more,
+			})
 		}
-		d = append(d, Category{
-			ID:     item.ID,
-			CID:    item.CID,
-			Title:  item.Title,
-			Cover:  item.Cover,
-			Region: item.Region,
-			More:   more,
-		})
+		leveldb.SetLevel(strings.Join([]string{"searchkey", key}, ":"), CateToStr(d), 86400000)
+	} else {
+		d = CateToJsons(cache)
 	}
 	ctx.JSON(d)
 }
