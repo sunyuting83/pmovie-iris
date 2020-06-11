c92de0d0c85fb25b3499be05a27916f6b848964f
sun <sleepsun123@163.com>
2020-06-11 23:40:23 +0800

    add cache to category

--- controller/category.go
+++ controller/category.go
@@ -2,8 +2,9 @@ package controller
 
 import (
 	"encoding/json"
-	"fmt"
+	leveldb "pornplay/leveldb"
 	model "pornplay/models"
+	"strconv"
 	"strings"
 
 	"github.com/kataras/iris"
@@ -40,35 +41,45 @@ func CategoryList(ctx iris.Context) {
 	if perr != nil {
 		page = 1
 	}
-	data, err := categoryList.GetCategory(category, page)
-	if err != nil {
-		fmt.Println("err")
-	}
+	cat := strconv.FormatInt(category, 10)
+	pa := strconv.FormatInt(page, 10)
 	d := make([]Category, 0)
-	for _, item := range data {
-		more := CategoryTojson(item.More)
-		// fmt.Println(more, item.More)
-		if !strings.Contains(item.Cover, "http") {
-			item.Cover = strings.Join([]string{"https", item.Cover}, ":")
+	cache := leveldb.GetLevel(strings.Join([]string{"category", cat, pa}, ":"))
+	if cache == "leveldb: not found" {
+		data, err := categoryList.GetCategory(category, page)
+		if err != nil {
+			d = make([]Category, 0)
 		}
-		if len(more.CIM) > 0 {
-			if !strings.Contains(more.CIM, "http") {
-				more.CIM = strings.Join([]string{"https", more.CIM}, ":")
+		for _, item := range data {
+			more := CategoryTojson(item.More)
+			// fmt.Println(more, item.More)
+			if !strings.Contains(item.Cover, "http") {
+				item.Cover = strings.Join([]string{"https", item.Cover}, ":")
 			}
-		}
-		if len(more.Play) > 0 {
-			if !strings.Contains(more.Play, "http") {
-				more.Play = strings.Join([]string{"http", more.Play}, ":")
+			if len(more.CIM) > 0 {
+				if !strings.Contains(more.CIM, "http") {
+					more.CIM = strings.Join([]string{"https", more.CIM}, ":")
+				}
 			}
+			if len(more.Play) > 0 {
+				if !strings.Contains(more.Play, "http") {
+					more.Play = strings.Join([]string{"http", more.Play}, ":")
+				}
+			}
+			d = append(d, Category{
+				ID:     item.ID,
+				CID:    item.CID,
+				Title:  item.Title,
+				Cover:  item.Cover,
+				Region: item.Region,
+				More:   more,
+			})
 		}
-		d = append(d, Category{
-			ID:     item.ID,
-			CID:    item.CID,
-			Title:  item.Title,
-			Cover:  item.Cover,
-			Region: item.Region,
-			More:   more,
-		})
+		if len(data) > 0 {
+			leveldb.SetLevel(strings.Join([]string{"category", cat, pa}, ":"), CateToStr(d), 86400000)
+		}
+	} else {
+		d = CateToJsons(cache)
 	}
 	ctx.JSON(iris.Map{"movie": d})
 }
@@ -77,6 +88,25 @@ func CategoryList(ctx iris.Context) {
 func CategoryTojson(s string) (p More) {
 	if err := json.Unmarshal([]byte(s), &p); err != nil {
 		// fmt.Println(err.Error())
+		return
+	}
+	return
+}
+
+// CateToJsons json
+func CateToJsons(s string) (result []Category) {
+	err := json.Unmarshal([]byte(s), &result)
+	if err != nil {
+		return
+	}
+	return
+}
+
+// CateToStr json
+func CateToStr(d []Category) (result string) {
+	resultByte, errError := json.Marshal(d)
+	result = string(resultByte)
+	if errError != nil {
 		return
 	}
 	return
