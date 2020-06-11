35d61b485fce4eef61822b9b9454b8e58e43fcca
sun <sleepsun123@163.com>
2020-06-11 23:25:03 +0800

    add cache to index

--- controller/index.go
+++ controller/index.go
@@ -1,7 +1,9 @@
 package controller
 
 import (
+	"encoding/json"
 	"fmt"
+	leveldb "pornplay/leveldb"
 	model "pornplay/models"
 	"strings"
 
@@ -14,14 +16,41 @@ var category model.Category
 func Indexs(ctx iris.Context) {
 	// fmt.Println(ctx.Host())
 	d := make([]model.Category, 0)
-	data, err := category.GetIndexs()
-	if err != nil {
-		fmt.Println("err")
-	}
-	host := strings.Join([]string{"http", ctx.Host()}, "://")
-	for _, item := range data {
-		item.Cover = strings.Join([]string{host, item.Cover}, "")
-		d = append(d, model.Category{ID: item.ID, Category: item.Category, Sort: item.Sort, Cover: item.Cover})
+	cache := leveldb.GetLevel("index")
+	if cache == "leveldb: not found" {
+		data, err := category.GetIndexs()
+		if err != nil {
+			fmt.Println("err")
+		}
+		host := strings.Join([]string{"http", ctx.Host()}, "://")
+		for _, item := range data {
+			item.Cover = strings.Join([]string{host, item.Cover}, "")
+			d = append(d, model.Category{ID: item.ID, Category: item.Category, Sort: item.Sort, Cover: item.Cover})
+		}
+		if len(data) > 0 {
+			leveldb.SetLevel("index", InToStr(d), 86400000)
+		}
+	} else {
+		d = InToJsons(cache)
 	}
 	ctx.JSON(iris.Map{"category": d})
+}
+
+// InToJsons index str to json
+func InToJsons(s string) (result []model.Category) {
+	err := json.Unmarshal([]byte(s), &result)
+	if err != nil {
+		return
+	}
+	return
+}
+
+// InToStr fun
+func InToStr(d []model.Category) (result string) {
+	resultByte, errError := json.Marshal(d)
+	result = string(resultByte)
+	if errError != nil {
+		return
+	}
+	return
 }
